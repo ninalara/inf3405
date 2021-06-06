db9db1e2711f82cc20e03fe5086cc8ef041fc75e
nina <liliamnina@gmail.com>
2021-06-06 13:32:33 -0400

    all commands work except for download/upload

--- tp1/Client/src/Client.java
+++ tp1/Client/src/Client.java
@@ -1,12 +1,20 @@
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.regex.Pattern;
 import java.net.Socket;
+import java.nio.file.Files;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
     private static Socket socket;
+	private static String clientPath = System.getProperty("user.dir") + "\\";
 
     public static void main(String[] args) throws Exception {
     	Scanner in = new Scanner(System.in);
@@ -36,9 +44,108 @@ public class Client {
 
 		String helloMessageFromServer = input.readUTF();
 		System.out.println(helloMessageFromServer);
-	    
+		while (true) {
+			System.out.print("> ");
+			String currentCommand = "";
+			currentCommand = in.nextLine();
+			if (currentCommand == "") continue;
+			output.writeUTF(currentCommand);
+			commandCompletion(input, output, currentCommand);
+			TimeUnit.MILLISECONDS.sleep(100);
+			while(input.available() != 0)
+			{
+				String serverComs = input.readUTF();
+				if (serverComs.isEmpty()) break;
+				System.out.println(serverComs);
+			}
+		}
     }
     
+	public static void commandCompletion(DataInputStream in, DataOutputStream out, String currentCommand) throws Exception {
+		String[] inputs = new String[] {};
+		try { inputs = currentCommand.split(" "); } catch(Exception e) {}
+
+		if (inputs.length == 0) return;
+			
+		switch(inputs[0]) {
+			case "upload":
+				uploadFile(out, inputs[1]);
+				break;
+			case "download":
+				downloadFile(in, inputs[1]);
+				break;
+			case "exit":
+				System.exit(0);
+				break;
+			default:
+				break;
+		}
+	}
+    
+    private static void uploadFile(DataOutputStream out, String fileName) throws IOException {
+   		
+    	File file = new File(clientPath + "\\" + fileName);
+    	System.out.print(file.toString());
+    	if(!(file.isFile())) {
+			return;
+		}
+		FileInputStream input = new FileInputStream(file.toString());
+		byte[] buffer = new byte[16*2024];
+		int fileDataSize = input.read();
+		int read = 0;
+		
+		while(fileDataSize > 0 && (read = input.read(buffer)) > 0) {
+			out.write(buffer, 0, read);
+			fileDataSize -= read;
+		}
+		input.close();
+    }
+    
+    // Download file from server from where server location to where client jar file is run.
+    private static void downloadFile(DataInputStream in, String fileName) throws Exception {
+    	
+		FileOutputStream fos = new FileOutputStream(clientPath + "\\" + fileName);
+		
+		//byte[] buffer = new byte[16*2024];
+		//byte[] buffer = in.readAllBytes();
+		
+		//long fileSize = in.readLong();
+		//System.out.print(fileSize);
+		//int read = 0;
+		
+		
+		//ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(clientPath + "\\" + fileName));
+		ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+		
+		//flush out
+		
+		if(in.readBoolean()) {
+			System.out.print(ois.available());
+			byte [] buffer = (byte[]) ois.readObject();
+			File downloadedFile = new File(clientPath + "\\" + fileName);
+			Files.write(downloadedFile.toPath(), buffer);
+			System.out.print(ois.readObject());
+		} else {
+			System.out.print("hey ptit queue ca marche pas!!");
+		}
+		
+		
+		//fos.write(buffer);
+		// While there is bytes in the in, we empty them in the file.
+//		while(fileSize > 0 && (read = in.read(buffer)) > 0) {
+//			fos.write(buffer, 0, read);
+//			fileSize -= read;
+//		}
+		//ois.writeObject(buffer);
+//		int count;
+//        while ((count = in.read(buffer)) > 0) {
+//            fos.write(buffer, 0, count);
+//        }
+		ois.close();
+		fos.close();
+    }    
+    
+	
 	private static final Pattern PATTERN = Pattern.compile(
 			"^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$"
 	);
--- tp1/Server/src/Server.java
+++ tp1/Server/src/Server.java
@@ -15,34 +15,34 @@ import java.util.List;
 import java.util.ArrayList;
 import java.io.FileOutputStream;
 
-
 public class Server {
 	private static ServerSocket listener;
 	private static String serverAddress = "127.0.0.1";
 	private static int port = 5000;
 	private static String serverPath = System.getProperty("user.dir") + "\\";
-    public static void main(String[] args) throws Exception {
-    	Scanner in = new Scanner(System.in);
+
+	public static void main(String[] args) throws Exception {
+		Scanner in = new Scanner(System.in);
 		System.out.println("Enter IP Address of the Server:");
 		serverAddress = in.nextLine();
-		while (!Server.validateIpAddress(serverAddress)){
+		while (!Server.validateIpAddress(serverAddress)) {
 			System.out.println("Wrong IP Address. Enter another one:");
-	        serverAddress = System.console().readLine();
-	    }
-			
-		//Port
-	    System.out.println("Enter Port for the server :");
-	    port = in.nextInt();
-	    while (!Server.validatePort(port)){
-	        System.out.println("Wrong Port. Should be between 5000 and 5050. Enter another one:");
-	        port = Integer.parseInt(System.console().readLine());
-	    }
+			serverAddress = System.console().readLine();
+		}
+
+		// Port
+		System.out.println("Enter Port for the server :");
+		port = in.nextInt();
+		while (!Server.validatePort(port)) {
+			System.out.println("Wrong Port. Should be between 5000 and 5050. Enter another one:");
+			port = Integer.parseInt(System.console().readLine());
+		}
 		InetAddress serverIP = InetAddress.getByName(serverAddress);
-	    
+
 		listener = new ServerSocket();
 		listener.setReuseAddress(true);
 		listener.bind(new InetSocketAddress(serverIP, port));
-		
+
 		try {
 			int nClients = -1;
 			while (true) {
@@ -52,14 +52,13 @@ public class Server {
 		} finally {
 			listener.close();
 		}
-		
-    }
-    
+
+	}
+
 //    https://stackoverflow.com/Questions/5667371/validate-ipv4-address-in-java
-	private static final Pattern PATTERN = Pattern.compile(
-			"^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$"
-	);
-    
+	private static final Pattern PATTERN = Pattern
+			.compile("^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+
 	public static boolean validatePort(final int port) {
 		return port >= 5000 && port <= 5500;
 	}
@@ -67,18 +66,18 @@ public class Server {
 	public static boolean validateIpAddress(final String ipAdress) {
 		return PATTERN.matcher(ipAdress).matches();
 	}
-	
-	
-    
+
 	private static class ClientHandler extends Thread {
 		private Socket socket;
 		private int clientNumber;
 		private String path = new String(serverPath);
+
 		public ClientHandler(Socket socket, int clientNumber) {
 			this.socket = socket;
 			this.clientNumber = clientNumber;
 			System.out.println("New connection with client#" + clientNumber + " at " + socket);
 		}
+
 		public void run() {
 			try {
 				DataInputStream in = new DataInputStream(socket.getInputStream());
@@ -87,28 +86,30 @@ public class Server {
 				commandSelector(in, out);
 			} catch (Exception e) {
 				System.out.println("Error handling client#" + clientNumber + ": " + e);
-			} 
+			}
 		}
+
 		public void commandSelector(DataInputStream in, DataOutputStream out) throws Exception {
 			int command = 0;
 			String line = "";
 			String[] inputs = new String[] {};
 			// wait for the client to enter a command
 			while (command == 0) {
+				
 				if (line == "") {
 					try {
 						// read the command that the client entered and split it
 						line = in.readUTF();
 						inputs = line.split(" ");
-						
-						// write in the server console everything needed 
+
+						// write in the server console everything needed
 						System.out.println("[" + serverAddress + ":" + port + " - " + java.time.LocalDate.now() + "@"
 								+ java.time.LocalTime.now().getHour() + ":" + java.time.LocalTime.now().getMinute()
 								+ ":" + java.time.LocalTime.now().getSecond() + "] : " + line);
 					} catch (Exception e) {
 					}
 				}
-				
+
 				if (inputs.length == 0)
 					continue;
 				// switch case with the different commands available
@@ -145,13 +146,15 @@ public class Server {
 				inputs = new String[] {};
 			}
 		}
+
 		public void cdCommand(DataOutputStream out, String[] inputs) throws Exception {
 			// verifies that the user has entered a name of directory
 			if (inputs.length == 1) {
 				out.writeUTF("No directory name was typed\n");
 				return;
 			} else if (inputs[1].equals("..")) {
-				// splits the path where there are "\" in the path if the second argument is ".."
+				// splits the path where there are "\" in the path if the second argument is
+				// ".."
 				String[] splitPath = path.split("\\\\");
 				String newPath = "";
 				// create a new path with all the split elements except for the last one
@@ -161,7 +164,7 @@ public class Server {
 				path = newPath;
 				System.out.println("HEAD on " + splitPath[splitPath.length - 2]);
 			} else if (!lsCommand(out, inputs, true).contains(inputs[1])) {
-				// error message if the directory is not found 
+				// error message if the directory is not found
 				out.writeUTF("No directory with that name was found\n");
 				return;
 			} else {
@@ -171,15 +174,15 @@ public class Server {
 			}
 			out.writeUTF("The current directory is now " + path);
 		}
-		
+
 		public void mkdirCommand(DataOutputStream out, String[] inputs) throws Exception {
 			// verifies that the client entered the name of the new directory
 			if (inputs.length == 1) {
 				out.writeUTF("No directory name was typed\n");
 				return;
 			}
-			
-			// Creates the new file with the right path 
+
+			// Creates the new file with the right path
 			File file = new File(path + inputs[1]);
 			// Created the directory
 			if (file.mkdir()) {
@@ -190,6 +193,7 @@ public class Server {
 				out.writeUTF("An Error has Occurred\n");
 			}
 		}
+
 		// From
 		// https://stackoverflow.com/questions/5694385/getting-the-filenames-of-all-files-in-a-folder#:~:text=Create%20a%20File%20object%2C%20passing,method%20to%20get%20the%20filename.
 		public List<String> lsCommand(DataOutputStream out, String[] inputs, boolean isCd) throws Exception {
@@ -197,7 +201,7 @@ public class Server {
 			// get all the files
 			File[] listOfFiles = currentFolder.listFiles();
 			List<String> directories = new ArrayList<String>();
-			// loop through all the files 
+			// loop through all the files
 			for (int i = 0; i < listOfFiles.length; i++) {
 				// JCOMPRENDS PAS CA
 				if (isCd) {
@@ -213,6 +217,7 @@ public class Server {
 			}
 			return directories;
 		}
+
 		public void uploadCommand(DataOutputStream out, String[] inputs) throws Exception {
 			// get the input stream form the socket
 			DataInputStream dis = new DataInputStream(socket.getInputStream());
@@ -230,8 +235,9 @@ public class Server {
 			dis.close();
 			fos.close();
 		}
+
 		private void downloadCommand(DataOutputStream out, String[] inputs) throws Exception {
-			// verifies that the user entered the name of the file to download 
+			// verifies that the user entered the name of the file to download
 			if (inputs.length == 1) {
 				out.writeUTF("No file name was typed\n");
 				return;
@@ -243,38 +249,38 @@ public class Server {
 				if (!(file.isFile())) {
 					out.writeUTF(fileName + " does not exist\n");
 					return;
-				}else {
+				} else {
 					byte[] buff = Files.readAllBytes(file.toPath());
-					ObjectOutputStream output =  new ObjectOutputStream(socket.getOutputStream());
-					//FileInputStream input = new FileInputStream(file.toString());
-					
-					//int fileDataSize = input.read();
+					ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+					// FileInputStream input = new FileInputStream(file.toString());
+
+					// int fileDataSize = input.read();
 					System.out.print(buff.length);
-					
+
 					output.writeObject(buff);
 //	    		while(fileDataSize > 0 && (read = input.read(buff)) > 0) {
 //	    			output.write(buff, 0, read);
 //	    			fileDataSize -= read;
 //	    		}
-					
+
 //	    		int count ;
 //	            while ((count = input.read(buff)) >= 0) {
 //	                out.write(buff, 0, count);
 //	                System.out.write(buff, 0, count);
 //	                
 //	            }
-					
+
 //	    		input.close();
 					output.close();
 //	    		System.out.println(fileName + " downloaded successfully.");	
-				}	
+				}
 			}
 		}
+
 		// prints the different commands that are available
 		private void helpCommand(DataOutputStream out) throws Exception {
-			out.writeUTF("ls : lists every files in current directory \ncd : change the current directory\nmkdir : create a new directory \ndownload : download a file from the Server to the Client \nupload : upload a file from the Client to the Server\n");
+			out.writeUTF(
+					"ls : lists every files in current directory \ncd : change the current directory\nmkdir : create a new directory \ndownload : download a file from the Server to the Client \nupload : upload a file from the Client to the Server\n");
 		}
 	}
 }
-
-
